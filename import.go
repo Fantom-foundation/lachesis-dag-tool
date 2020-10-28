f43f697e8e4f9775bcb8ea01a8d1326150746eda
alex <rusdev.alex@gmail.com>
2020-10-28 13:21:28 +1000

    cli command 'import'

---
+++ import.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"context"
+	"strconv"
+
+	"github.com/Fantom-foundation/go-lachesis/inter/idx"
+	"github.com/urfave/cli"
+
+	"github.com/Fantom-foundation/lachesis-dag-tool/presentation"
+	"github.com/Fantom-foundation/lachesis-dag-tool/source"
+)
+
+var (
+	dataDirFlag = cli.StringFlag{
+		Name:  "datadir",
+		Usage: "Data directory of lachesis node",
+		Value: source.DefaultDataDir(),
+	}
+
+	cmdImport = cli.Command{
+		Name:      "import",
+		ShortName: "i",
+		Flags: []cli.Flag{
+			dataDirFlag,
+		},
+		Action: actImport,
+		Usage:  "Import DAG from lachesis datadir into Neo4j.",
+	}
+)
+
+func actImport(cli *cli.Context) (err error) {
+	src := cli.GlobalString(dataDirFlag.Name)
+	dst := cli.GlobalString(neo4jUrlFlag.Name)
+	from := idx.Epoch(1)
+	if len(cli.Args()) > 1 {
+		n, err := strconv.ParseUint(cli.Args().Get(1), 10, 32)
+		if err != nil {
+			return err
+		}
+		from = idx.Epoch(n)
+	}
+	to := idx.Epoch(0)
+	if len(cli.Args()) > 2 {
+		n, err := strconv.ParseUint(cli.Args().Get(2), 10, 32)
+		if err != nil {
+			return err
+		}
+		to = idx.Epoch(n)
+	}
+
+	ctx, stop := context.WithCancel(context.Background())
+	go func() {
+		defer stop()
+		events := source.Events(ctx, src, from, to)
+		err = presentation.LoadToNeo4j(dst, events)
+	}()
+
+	waitForInterrupt(ctx)
+	return
+}
--- main.go
+++ main.go
@@ -4,16 +4,13 @@ import (
 	"context"
 	"os"
 	"os/signal"
-	"strconv"
 	"syscall"
 
-	"github.com/Fantom-foundation/go-lachesis/inter/idx"
 	"github.com/ethereum/go-ethereum/log"
 	"github.com/ethereum/go-ethereum/params"
 	"github.com/urfave/cli"
 
 	"github.com/Fantom-foundation/lachesis-dag-tool/presentation"
-	"github.com/Fantom-foundation/lachesis-dag-tool/source"
 )
 
 var (
@@ -23,11 +20,6 @@ var (
 	// App that holds all commands and flags.
 	App = cli.NewApp()
 
-	dataDirFlag = cli.StringFlag{
-		Name:  "datadir",
-		Usage: "Data directory of lachesis node",
-		Value: source.DefaultDataDir(),
-	}
 	neo4jUrlFlag = cli.StringFlag{
 		Name:  "neo4j",
 		Usage: "Neo4j DB url",
@@ -47,41 +39,11 @@ func version() string {
 func init() {
 	App.Version = version()
 	App.Flags = []cli.Flag{
-		dataDirFlag,
 		neo4jUrlFlag,
 	}
-	App.Action = cmdDefault
-}
-
-func cmdDefault(cli *cli.Context) (err error) {
-	src := cli.GlobalString(dataDirFlag.Name)
-	dst := cli.GlobalString(neo4jUrlFlag.Name)
-	from := idx.Epoch(1)
-	if len(cli.Args()) > 1 {
-		n, err := strconv.ParseUint(cli.Args().Get(1), 10, 32)
-		if err != nil {
-			return err
-		}
-		from = idx.Epoch(n)
+	App.Commands = []cli.Command{
+		cmdImport,
 	}
-	to := idx.Epoch(0)
-	if len(cli.Args()) > 2 {
-		n, err := strconv.ParseUint(cli.Args().Get(2), 10, 32)
-		if err != nil {
-			return err
-		}
-		to = idx.Epoch(n)
-	}
-
-	ctx, cancel := context.WithCancel(context.Background())
-	go func() {
-		defer cancel()
-		events := source.Events(ctx, src, from, to)
-		err = presentation.LoadToNeo4j(dst, events)
-	}()
-
-	waitForInterrupt(ctx)
-	return
 }
 
 func waitForInterrupt(ctx context.Context) {
