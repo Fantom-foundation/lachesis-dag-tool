35b78402d596911887c372747e8021b5b9c86ace
alex <rusdev.alex@gmail.com>
2021-01-20 22:22:32 +1000

    p2p network discovery

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.14
 
 require (
 	github.com/Fantom-foundation/go-lachesis v0.8.0-rc.2
+	github.com/deckarep/golang-set v1.7.1
 	github.com/ethereum/go-ethereum v1.9.8
 	github.com/neo4j/neo4j-go-driver v1.8.3
 	github.com/paulbellamy/ratecounter v0.2.0
@@ -14,4 +15,5 @@ require (
 replace (
 	github.com/ethereum/go-ethereum => github.com/Fantom-Foundation/go-ethereum v1.9.22-ftm-0.1
 	gopkg.in/urfave/cli.v1 => github.com/urfave/cli v1.20.0
+	github.com/Fantom-foundation/go-lachesis => ../go-lachesis
 )
--- source/p2p.go
+++ source/p2p.go
@@ -2,21 +2,35 @@ package source
 
 import (
 	"context"
+	"io/ioutil"
+	"sync"
 
+	"github.com/Fantom-foundation/go-lachesis/gossip"
 	"github.com/Fantom-foundation/go-lachesis/inter"
 	"github.com/Fantom-foundation/go-lachesis/inter/idx"
+	"github.com/Fantom-foundation/go-lachesis/kvdb/memorydb"
+	"github.com/Fantom-foundation/go-lachesis/lachesis"
+	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/log"
 	"github.com/ethereum/go-ethereum/node"
+	"github.com/ethereum/go-ethereum/p2p"
+	"github.com/ethereum/go-ethereum/p2p/discv5"
+	"github.com/ethereum/go-ethereum/p2p/enode"
+	"github.com/ethereum/go-ethereum/p2p/enr"
 )
 
 func EventsFromP2p(ctx context.Context, network string, from, to idx.Epoch) <-chan *inter.Event {
 	log.Info("Events of epoches", "from", from, "to", to, "network", network)
 	output := make(chan *inter.Event, 10)
 
+	svc := newService(network, output)
+	stack := newStack()
+	stack.RegisterProtocols(svc.Protocols())
+	stack.RegisterLifecycle(svc)
+	svc.p2pServer = stack.Server()
+
 	go func() {
 		defer close(output)
-
-		stack := makeStack(output)
 
 		err := stack.Start()
 		if err != nil {
@@ -31,7 +45,152 @@ func EventsFromP2p(ctx context.Context, network string, from, to idx.Epoch) <-ch
 	return output
 }
 
-func makeStack(chan<- *inter.Event) *node.Node {
-	// TODO: make it
+func newStack() *node.Node {
+	var cfg node.Config
+	tmpDir, err := ioutil.TempDir("", "")
+	if err != nil {
+		panic(err)
+	}
+	cfg.DataDir = tmpDir
+
+	stack, err := node.New(&cfg)
+	if err != nil {
+		panic(err)
+	}
+
+	return stack
+}
+
+type service struct {
+	net     lachesis.Config
+	genesis common.Hash
+	output  chan<- *inter.Event
+
+	// server
+	p2pServer  *p2p.Server
+	serverPool *gossip.ServerPool
+
+	done chan struct{}
+	wg   sync.WaitGroup
+}
+
+func newService(network string, output chan<- *inter.Event) *service {
+	var (
+		net     lachesis.Config
+		genesis common.Hash
+	)
+	switch network {
+	case "test":
+		net = lachesis.TestNetConfig()
+		genesis = common.HexToHash("0x5e521b3244c5ffb02f5c54e8f69e6441da12cb7ed8a73d8a1ddcb94008acd597")
+	case "main":
+		net = lachesis.MainNetConfig()
+		genesis = common.HexToHash("0xe7813c633b1023bcc132f45273a69df9f3847f16b3209df08517d20bcef3d580")
+	default:
+		panic("unknow network " + network)
+	}
+
+	svc := &service{
+		net:     net,
+		genesis: genesis,
+		output:  output,
+
+		done: make(chan struct{}),
+	}
+
+	trustedNodes := []string{}
+	db := memorydb.New()
+	svc.serverPool = gossip.NewServerPool(db, svc.done, &svc.wg, trustedNodes)
+
+	return svc
+}
+
+// Start is called after all services have been constructed and the networking
+// layer was also initialized to spawn any goroutines required by the service.
+func (s *service) Start() error {
+	topic := discv5.Topic("lachesis@" + s.genesis.Hex())
+	if s.p2pServer.DiscV5 != nil {
+		go func(topic discv5.Topic) {
+			log.Info("Starting topic registration")
+			defer log.Info("Terminated topic registration")
+
+			s.p2pServer.DiscV5.RegisterTopic(topic, s.done)
+		}(topic)
+	}
+
+	s.serverPool.Start(s.p2pServer, topic)
+
 	return nil
+}
+
+// Stop terminates all goroutines belonging to the service, blocking until they
+// are all terminated.
+func (s *service) Stop() error {
+	close(s.done)
+	s.wg.Wait()
+	return nil
+}
+
+// Protocols returns protocols the service can communicate on.
+func (s *service) Protocols() []p2p.Protocol {
+	protos := make([]p2p.Protocol, len(gossip.ProtocolVersions))
+	for i, vsn := range gossip.ProtocolVersions {
+		protos[i] = s.makeProtocol(vsn)
+		protos[i].Attributes = []enr.Entry{&gossip.Enr{}}
+	}
+	return protos
+}
+
+func (s *service) makeProtocol(version uint) p2p.Protocol {
+	return p2p.Protocol{
+		Name:    "lachesis",
+		Version: version,
+		Length:  gossip.PackMsg + 1,
+		Run: func(p *p2p.Peer, rw p2p.MsgReadWriter) error {
+			var entry *gossip.PoolEntry
+			peer := gossip.NewPeer(int(version), p, rw)
+			if s.serverPool != nil {
+				entry = s.serverPool.Connect(peer, peer.Node())
+			}
+			peer.PoolEntry = entry
+			/*
+				select {
+				case pm.newPeerCh <- peer:
+					pm.wg.Add(1)
+					defer pm.wg.Done()
+					err := pm.handle(peer)
+					if entry != nil {
+						s.serverPool.Disconnect(entry)
+					}
+					return err
+				case <-pm.quitSync:
+					if entry != nil {
+						s.serverPool.Disconnect(entry)
+					}
+					return p2p.DiscQuitting
+				}
+			*/
+			log.Warn("Connect to node", "addr", peer.RemoteAddr().String())
+			if entry != nil {
+				s.serverPool.Disconnect(entry)
+			}
+			return p2p.DiscQuitting
+		},
+		NodeInfo: func() interface{} {
+			return s.NodeInfo()
+		},
+		PeerInfo: func(id enode.ID) interface{} {
+			return nil
+		},
+	}
+}
+
+// NodeInfo retrieves some protocol metadata about the running host node.
+func (s *service) NodeInfo() *gossip.NodeInfo {
+	return &gossip.NodeInfo{
+		Network:     s.net.NetworkID,
+		Genesis:     s.genesis,
+		Epoch:       1,
+		NumOfBlocks: 0,
+	}
 }
