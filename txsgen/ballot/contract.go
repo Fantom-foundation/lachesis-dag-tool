57ba94899a716c35d22957ab022750c24cd19e87
alex <rusdev.alex@gmail.com>
2021-02-27 16:04:18 +1000

    Ballot contract: Voiting event

--- txsgen/ballot/contract.go
+++ txsgen/ballot/contract.go
@@ -28,10 +28,10 @@ var (
 )
 
 // ContractABI is the input ABI used to generate the binding from.
-const ContractABI = "[{\"inputs\":[{\"internalType\":\"bytes32[]\",\"name\":\"proposalNames\",\"type\":\"bytes32[]\"}],\"payable\":false,\"stateMutability\":\"nonpayable\",\"type\":\"constructor\"},{\"constant\":true,\"inputs\":[{\"internalType\":\"uint256\",\"name\":\"\",\"type\":\"uint256\"}],\"name\":\"proposals\",\"outputs\":[{\"internalType\":\"bytes32\",\"name\":\"name\",\"type\":\"bytes32\"},{\"internalType\":\"uint256\",\"name\":\"voteCount\",\"type\":\"uint256\"}],\"payable\":false,\"stateMutability\":\"view\",\"type\":\"function\"},{\"constant\":false,\"inputs\":[{\"internalType\":\"uint256\",\"name\":\"proposal\",\"type\":\"uint256\"}],\"name\":\"vote\",\"outputs\":[],\"payable\":false,\"stateMutability\":\"nonpayable\",\"type\":\"function\"},{\"constant\":true,\"inputs\":[{\"internalType\":\"address\",\"name\":\"\",\"type\":\"address\"}],\"name\":\"voters\",\"outputs\":[{\"internalType\":\"uint256\",\"name\":\"vote\",\"type\":\"uint256\"}],\"payable\":false,\"stateMutability\":\"view\",\"type\":\"function\"},{\"constant\":true,\"inputs\":[],\"name\":\"winnerName\",\"outputs\":[{\"internalType\":\"bytes32\",\"name\":\"winnerName_\",\"type\":\"bytes32\"}],\"payable\":false,\"stateMutability\":\"view\",\"type\":\"function\"},{\"constant\":true,\"inputs\":[],\"name\":\"winningProposal\",\"outputs\":[{\"internalType\":\"uint256\",\"name\":\"winningProposal_\",\"type\":\"uint256\"}],\"payable\":false,\"stateMutability\":\"view\",\"type\":\"function\"}]"
+const ContractABI = "[{\"inputs\":[{\"internalType\":\"bytes32[]\",\"name\":\"proposalNames\",\"type\":\"bytes32[]\"}],\"payable\":false,\"stateMutability\":\"nonpayable\",\"type\":\"constructor\"},{\"anonymous\":false,\"inputs\":[{\"indexed\":true,\"internalType\":\"address\",\"name\":\"who\",\"type\":\"address\"},{\"indexed\":true,\"internalType\":\"uint256\",\"name\":\"num\",\"type\":\"uint256\"},{\"indexed\":true,\"internalType\":\"bytes32\",\"name\":\"text\",\"type\":\"bytes32\"},{\"indexed\":false,\"internalType\":\"uint256\",\"name\":\"amount\",\"type\":\"uint256\"}],\"name\":\"Voiting\",\"type\":\"event\"},{\"constant\":true,\"inputs\":[{\"internalType\":\"uint256\",\"name\":\"\",\"type\":\"uint256\"}],\"name\":\"proposals\",\"outputs\":[{\"internalType\":\"bytes32\",\"name\":\"name\",\"type\":\"bytes32\"},{\"internalType\":\"uint256\",\"name\":\"voteCount\",\"type\":\"uint256\"}],\"payable\":false,\"stateMutability\":\"view\",\"type\":\"function\"},{\"constant\":false,\"inputs\":[{\"internalType\":\"uint256\",\"name\":\"proposal\",\"type\":\"uint256\"}],\"name\":\"vote\",\"outputs\":[],\"payable\":true,\"stateMutability\":\"payable\",\"type\":\"function\"},{\"constant\":true,\"inputs\":[{\"internalType\":\"address\",\"name\":\"\",\"type\":\"address\"}],\"name\":\"voters\",\"outputs\":[{\"internalType\":\"uint256\",\"name\":\"vote\",\"type\":\"uint256\"}],\"payable\":false,\"stateMutability\":\"view\",\"type\":\"function\"},{\"constant\":true,\"inputs\":[],\"name\":\"winnerName\",\"outputs\":[{\"internalType\":\"bytes32\",\"name\":\"winnerName_\",\"type\":\"bytes32\"}],\"payable\":false,\"stateMutability\":\"view\",\"type\":\"function\"},{\"constant\":true,\"inputs\":[],\"name\":\"winningProposal\",\"outputs\":[{\"internalType\":\"uint256\",\"name\":\"winningProposal_\",\"type\":\"uint256\"}],\"payable\":false,\"stateMutability\":\"view\",\"type\":\"function\"}]"
 
 // ContractBin is the compiled bytecode used for deploying new contracts.
-var ContractBin = "0x608060405234801561001057600080fd5b506040516103ce3803806103ce8339818101604052602081101561003357600080fd5b810190808051604051939291908464010000000082111561005357600080fd5b90830190602082018581111561006857600080fd5b825186602082028301116401000000008211171561008557600080fd5b82525081516020918201928201910280838360005b838110156100b257818101518382015260200161009a565b5050505090500160405250505060008090505b815181101561012757600160405180604001604052808484815181106100e757fe5b60209081029190910181015182526000918101829052835460018181018655948352918190208351600290930201918255919091015190820155016100c5565b5050610296806101386000396000f3fe608060405234801561001057600080fd5b50600436106100675760003560e01c8063609ff1bd11610050578063609ff1bd146100c1578063a3ec138d146100db578063e2ba53f01461010e57610067565b80630121b93f1461006c578063013cf08b1461008b575b600080fd5b6100896004803603602081101561008257600080fd5b5035610116565b005b6100a8600480360360208110156100a157600080fd5b5035610190565b6040805192835260208301919091528051918290030190f35b6100c96101bb565b60408051918252519081900360200190f35b6100c9600480360360208110156100f157600080fd5b503573ffffffffffffffffffffffffffffffffffffffff16610222565b6100c9610234565b33600090815260208190526040902054801561015957600180600183038154811061013d57fe5b6000918252602090912060016002909202010180549190910390555b3360009081526020526001805481908490811061017257fe5b60009182526020909120600160029092020101805490910190555050565b6001818154811061019d57fe5b60009182526020909120600290910201805460019091015490915082565b600080805b60015481101561021d5781600182815481106101d857fe5b906000526020600020906002020160010154111561021557600181815481106101fd57fe5b90600052602060002090600202016001015491508092505b6001016101c0565b505090565b60006020819052908152604090205481565b600060016102406101bb565b8154811061024a57fe5b90600052602060002090600202016000015490509056fea265627a7a7231582037742a904c1b3c354607cb8720e53be9c243f739af54d1a010404be2735407e664736f6c634300050c0032"
+var ContractBin = "0x608060405234801561001057600080fd5b506040516104673803806104678339818101604052602081101561003357600080fd5b810190808051604051939291908464010000000082111561005357600080fd5b90830190602082018581111561006857600080fd5b825186602082028301116401000000008211171561008557600080fd5b82525081516020918201928201910280838360005b838110156100b257818101518382015260200161009a565b5050505090500160405250505060008090505b815181101561012757600160405180604001604052808484815181106100e757fe5b60209081029190910181015182526000918101829052835460018181018655948352918190208351600290930201918255919091015190820155016100c5565b505061032f806101386000396000f3fe60806040526004361061005a5760003560e01c8063609ff1bd11610043578063609ff1bd146100c1578063a3ec138d146100e8578063e2ba53f0146101285761005a565b80630121b93f1461005f578063013cf08b1461007e575b600080fd5b61007c6004803603602081101561007557600080fd5b503561013d565b005b34801561008a57600080fd5b506100a8600480360360208110156100a157600080fd5b5035610229565b6040805192835260208301919091528051918290030190f35b3480156100cd57600080fd5b506100d6610254565b60408051918252519081900360200190f35b3480156100f457600080fd5b506100d66004803603602081101561010b57600080fd5b503573ffffffffffffffffffffffffffffffffffffffff166102bb565b34801561013457600080fd5b506100d66102cd565b33600090815260208190526040902054801561018057600180600183038154811061016457fe5b6000918252602090912060016002909202010180549190910390555b3360009081526020526001805481908490811061019957fe5b906000526020600020906002020160010160008282540192505081905550600182815481106101c457fe5b906000526020600020906002020160000154823373ffffffffffffffffffffffffffffffffffffffff167fee51d93e4784cfee8c8b14400fd5c768d31980d6a648124618b164b9b3dd69e1346040518082815260200191505060405180910390a45050565b6001818154811061023657fe5b60009182526020909120600290910201805460019091015490915082565b600080805b6001548110156102b657816001828154811061027157fe5b90600052602060002090600202016001015411156102ae576001818154811061029657fe5b90600052602060002090600202016001015491508092505b600101610259565b505090565b60006020819052908152604090205481565b600060016102d9610254565b815481106102e357fe5b90600052602060002090600202016000015490509056fea265627a7a723158207d2c2287159dd480a6fc9af9edeae1b086492978db89fbc32be265a54a9c00d764736f6c634300050c0032"
 
 // DeployContract deploys a new Ethereum contract, binding an instance of Contract to it.
 func DeployContract(auth *bind.TransactOpts, backend bind.ContractBackend, proposalNames [][32]byte) (common.Address, *types.Transaction, *Contract, error) {
@@ -322,4 +322,166 @@ func (_Contract *ContractSession) Vote(proposal *big.Int) (*types.Transaction, e
 // Solidity: function vote(uint256 proposal) returns()
 func (_Contract *ContractTransactorSession) Vote(proposal *big.Int) (*types.Transaction, error) {
 	return _Contract.Contract.Vote(&_Contract.TransactOpts, proposal)
+}
+
+// ContractVoitingIterator is returned from FilterVoiting and is used to iterate over the raw logs and unpacked data for Voiting events raised by the Contract contract.
+type ContractVoitingIterator struct {
+	Event *ContractVoiting // Event containing the contract specifics and raw log
+
+	contract *bind.BoundContract // Generic contract to use for unpacking event data
+	event    string              // Event name to use for unpacking event data
+
+	logs chan types.Log        // Log channel receiving the found contract events
+	sub  ethereum.Subscription // Subscription for errors, completion and termination
+	done bool                  // Whether the subscription completed delivering logs
+	fail error                 // Occurred error to stop iteration
+}
+
+// Next advances the iterator to the subsequent event, returning whether there
+// are any more events found. In case of a retrieval or parsing error, false is
+// returned and Error() can be queried for the exact failure.
+func (it *ContractVoitingIterator) Next() bool {
+	// If the iterator failed, stop iterating
+	if it.fail != nil {
+		return false
+	}
+	// If the iterator completed, deliver directly whatever's available
+	if it.done {
+		select {
+		case log := <-it.logs:
+			it.Event = new(ContractVoiting)
+			if err := it.contract.UnpackLog(it.Event, it.event, log); err != nil {
+				it.fail = err
+				return false
+			}
+			it.Event.Raw = log
+			return true
+
+		default:
+			return false
+		}
+	}
+	// Iterator still in progress, wait for either a data or an error event
+	select {
+	case log := <-it.logs:
+		it.Event = new(ContractVoiting)
+		if err := it.contract.UnpackLog(it.Event, it.event, log); err != nil {
+			it.fail = err
+			return false
+		}
+		it.Event.Raw = log
+		return true
+
+	case err := <-it.sub.Err():
+		it.done = true
+		it.fail = err
+		return it.Next()
+	}
+}
+
+// Error returns any retrieval or parsing error occurred during filtering.
+func (it *ContractVoitingIterator) Error() error {
+	return it.fail
+}
+
+// Close terminates the iteration process, releasing any pending underlying
+// resources.
+func (it *ContractVoitingIterator) Close() error {
+	it.sub.Unsubscribe()
+	return nil
+}
+
+// ContractVoiting represents a Voiting event raised by the Contract contract.
+type ContractVoiting struct {
+	Who    common.Address
+	Num    *big.Int
+	Text   [32]byte
+	Amount *big.Int
+	Raw    types.Log // Blockchain specific contextual infos
+}
+
+// FilterVoiting is a free log retrieval operation binding the contract event 0xee51d93e4784cfee8c8b14400fd5c768d31980d6a648124618b164b9b3dd69e1.
+//
+// Solidity: event Voiting(address indexed who, uint256 indexed num, bytes32 indexed text, uint256 amount)
+func (_Contract *ContractFilterer) FilterVoiting(opts *bind.FilterOpts, who []common.Address, num []*big.Int, text [][32]byte) (*ContractVoitingIterator, error) {
+
+	var whoRule []interface{}
+	for _, whoItem := range who {
+		whoRule = append(whoRule, whoItem)
+	}
+	var numRule []interface{}
+	for _, numItem := range num {
+		numRule = append(numRule, numItem)
+	}
+	var textRule []interface{}
+	for _, textItem := range text {
+		textRule = append(textRule, textItem)
+	}
+
+	logs, sub, err := _Contract.contract.FilterLogs(opts, "Voiting", whoRule, numRule, textRule)
+	if err != nil {
+		return nil, err
+	}
+	return &ContractVoitingIterator{contract: _Contract.contract, event: "Voiting", logs: logs, sub: sub}, nil
+}
+
+// WatchVoiting is a free log subscription operation binding the contract event 0xee51d93e4784cfee8c8b14400fd5c768d31980d6a648124618b164b9b3dd69e1.
+//
+// Solidity: event Voiting(address indexed who, uint256 indexed num, bytes32 indexed text, uint256 amount)
+func (_Contract *ContractFilterer) WatchVoiting(opts *bind.WatchOpts, sink chan<- *ContractVoiting, who []common.Address, num []*big.Int, text [][32]byte) (event.Subscription, error) {
+
+	var whoRule []interface{}
+	for _, whoItem := range who {
+		whoRule = append(whoRule, whoItem)
+	}
+	var numRule []interface{}
+	for _, numItem := range num {
+		numRule = append(numRule, numItem)
+	}
+	var textRule []interface{}
+	for _, textItem := range text {
+		textRule = append(textRule, textItem)
+	}
+
+	logs, sub, err := _Contract.contract.WatchLogs(opts, "Voiting", whoRule, numRule, textRule)
+	if err != nil {
+		return nil, err
+	}
+	return event.NewSubscription(func(quit <-chan struct{}) error {
+		defer sub.Unsubscribe()
+		for {
+			select {
+			case log := <-logs:
+				// New log arrived, parse the event and forward to the user
+				event := new(ContractVoiting)
+				if err := _Contract.contract.UnpackLog(event, "Voiting", log); err != nil {
+					return err
+				}
+				event.Raw = log
+
+				select {
+				case sink <- event:
+				case err := <-sub.Err():
+					return err
+				case <-quit:
+					return nil
+				}
+			case err := <-sub.Err():
+				return err
+			case <-quit:
+				return nil
+			}
+		}
+	}), nil
+}
+
+// ParseVoiting is a log parse operation binding the contract event 0xee51d93e4784cfee8c8b14400fd5c768d31980d6a648124618b164b9b3dd69e1.
+//
+// Solidity: event Voiting(address indexed who, uint256 indexed num, bytes32 indexed text, uint256 amount)
+func (_Contract *ContractFilterer) ParseVoiting(log types.Log) (*ContractVoiting, error) {
+	event := new(ContractVoiting)
+	if err := _Contract.contract.UnpackLog(event, "Voiting", log); err != nil {
+		return nil, err
+	}
+	return event, nil
 }
